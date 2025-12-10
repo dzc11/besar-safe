f8c956902b5d8948af8b128d3677bf9eafa76369
dzc11 <ahterastudio@gmail.com>
2025-12-10 16:00:29 +0700

    Update tailwind.config.js

--- fronted/src/tailwind.config.js
+++ fronted/src/tailwind.config.js
@@ -1,1 +1,17 @@
-
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,jsx}',
+    './src/components/**/*.{js,jsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#3396bb',
+      },
+      borderRadius: {
+        xl: '1rem'
+      }
+    },
+  },
+  plugins: [],
+}
