b00bc668b19fef0510cfbb279b2d5842afd31e94
dzc11 <ahterastudio@gmail.com>
2025-12-10 16:01:31 +0700

    Update _app.jsx

--- fronted/src/pages/_app.jsx
+++ fronted/src/pages/_app.jsx
@@ -1,1 +1,22 @@
+import '../styles/globals.css'
+import { useEffect } from 'react'
+import Head from 'next/head'
 
+export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // fix leaflet icon path when used with Next.js
+    if (typeof window !== 'undefined') {
+      import('leaflet/dist/leaflet.css')
+    }
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>BesarSafe - Crowd Flood Reporting for Kelurahan Besar</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
