5b530955cf75c8fe4a7373b2530b6b0c286e9361
dzc11 <ahterastudio@gmail.com>
2025-12-10 16:02:35 +0700

    Update useStore.js

--- fronted/src/store/useStore.js
+++ fronted/src/store/useStore.js
@@ -1,1 +1,8 @@
+import create from 'zustand'
 
+export const useStore = create(set => ({
+  userPhone: null,
+  setUserPhone: (p) => set({ userPhone: p }),
+  reports: [],
+  setReports: (r) => set({ reports: r })
+}));
