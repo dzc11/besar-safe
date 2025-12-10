4da6ca8e746e4b9c7e662b48f3225e7b1b7309f4
dzc11 <ahterastudio@gmail.com>
2025-12-10 16:04:13 +0700

    Update FloatingButton.jsx

--- fronted/src/components/FloatingButton.jsx
+++ fronted/src/components/FloatingButton.jsx
@@ -1,1 +1,12 @@
-
+import Link from 'next/link'
+export default function FloatingButton() {
+  return (
+    <div className="fixed bottom-6 right-6">
+      <Link href="/report">
+        <a className="bg-primary text-white p-4 rounded-full shadow-xl inline-flex items-center justify-center">
+          + Report
+        </a>
+      </Link>
+    </div>
+  )
+}
